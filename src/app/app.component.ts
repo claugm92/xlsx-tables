1d88edb86df01ca52e2402347acd5b824e1709f2
Claudio Montañez <claugm92@mgmail.com>
2022-06-13 18:34:51 -0300

    export funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,20 +52,6 @@ export class AppComponent implements OnInit {
     });
   }
 
-  onUpload(event: any) {
-    for (let file of event.files) {
-      this.uploadedFiles.push(file);
-    }
-
-    console.log(this.uploadedFiles);
-
-    this.messageService.add({
-      severity: 'info',
-      summary: 'File Uploaded',
-      detail: '',
-    });
-  }
-
   onFileChange(event: any) {
     for (let file of event.files) {
       this.uploadedFiles.push(file);
@@ -81,6 +67,7 @@ export class AppComponent implements OnInit {
         workBook = xlsx.read(data, { type: 'binary' });
         jsonData = workBook.SheetNames.reduce((initial: any, name: any) => {
           const sheet = workBook.Sheets[name];
+          console.log(sheet);
           initial[name] = xlsx.utils.sheet_to_json(sheet);
           return initial;
         }, {});
@@ -92,65 +79,31 @@ export class AppComponent implements OnInit {
     });
   }
 
-  // onSelect(event: any) {
-  //   for (let file of event.files) {
-  //     this.uploadedFiles.push(file);
-  //   }
-
-  //   // let fileReader = new FileReader();
-  //   // fileReader.onload = (e) => {
-  //   //   console.log(fileReader.result);
-  //   // };
-  //   // fileReader.readAsText(this.uploadedFiles[0]);
-
-  //   // console.log(this.uploadedFiles);
-
-  //   import('xlsx').then((xlsx) => {
-  //     let readAsDefault: 'ArrayBuffer'
-  //     const worksheet = xlsx.utils.sheet_to_json(this.uploadedFiles[0]);
-  //     const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-  //     console.log(workbook.Sheets);
-  //   });
-
-  //   // import('xlsx').then((xlsx) => {
-  //   //   var workbook = xlsx.readFile(this.uploadedFiles[0]);
-  //   //   var sheet_name_list = workbook.SheetNames;
-  //   //   console.log(
-  //   //     xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
-  //   //   );
-  //   // });
-
-  //   this.messageService.add({
-  //     severity: 'info',
-  //     summary: 'File Uploaded',
-  //     detail: '',
-  //   });
-  // }
-
   exportExcel() {
     import('xlsx').then((xlsx) => {
-      const worksheet = xlsx.utils.json_to_sheet(this.products);
+      const worksheet = xlsx.utils.json_to_sheet(this.output);
       const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      console.log(workbook);
       const excelBuffer: any = xlsx.write(workbook, {
         bookType: 'xlsx',
         type: 'array',
       });
-      this.saveAsExcelFile(excelBuffer, 'products');
+      this.saveAsExcelFile(excelBuffer, 'output');
     });
   }
 
   saveAsExcelFile(buffer: any, fileName: string): void {
-    import('file-saver').then((FileSaver) => {
-      let EXCEL_TYPE =
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-      let EXCEL_EXTENSION = '.xlsx';
-      const data: Blob = new Blob([buffer], {
-        type: EXCEL_TYPE,
-      });
-      FileSaver.saveAs(
-        data,
-        fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
-      );
+    // import('file-saver').then((FileSaver) => {
+    let EXCEL_TYPE =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE,
     });
+    FileSaver.saveAs(
+      data,
+      fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
+    );
+    // });
   }
 }
